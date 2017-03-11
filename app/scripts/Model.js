6c8a775e877372317c5d2f91bdab930faa8752a2
ReggieLai <Regina@dhcp-206-12-45-208.ubcsecure.wireless.ubc.ca>
2017-03-11 11:45:13 -0800

    Stressed path

--- app/scripts/Model.js
+++ app/scripts/Model.js
@@ -1,8 +1,9 @@
 const STAGE_TYPE = {
     MC_TYPE: 'mc_type',
     TEXT_TYPE: 'text_type',
-    MEDIA_TYPE: 'media_type',
+    VIDEO_TYPE: 'media_type',
     AUDIO_TYPE: 'audio_type'
+    PIC_TYPE: 'pic_type'
 };
 
 
@@ -13,7 +14,7 @@ class HappyKitModel {
             prev: null,
             next: null,
             title: 'BreatheStage',
-            content: {    
+            content: {
                 choices: []
             }
         };
@@ -23,7 +24,7 @@ class HappyKitModel {
             prev: null,
             next: null,
             title: 'VisualStage',
-            content: {    
+            content: {
                 choices: []
             }
         };
@@ -33,31 +34,80 @@ class HappyKitModel {
             prev: null,
             next: null,
             title: 'CuteVidStage',
-            content: {    
+            content: {
                 choices: []
             }
         };
 
-        this.initStage = {    
+        this.initStage = {
             type: STAGE_TYPE.MC_TYPE,
             prev: null,
             next: [this.BreatheStage, this.VisualStage, this.CuteVidStage],
             title: 'How are you feeling today?',
-            content: {    
+            content: {
                 choices: [{
                     text: 'Sad',
                     icon: 'http://sadface.png',
                     nextIndex: 2
                 }, {
                     text: 'Anxious',
-                    icon: '', 
+                    icon: '',
                     nextIndex: 0
                 }, {
-                    text: 'Stressed', 
-                    icon: '', 
+                    text: 'Stressed',
+                    icon: '',
                     nextIndex: 1
                 }]
             }
-        }; 
+        };
+
+        this.VisualizationStage = {
+          type: STAGE_TYPE.TEXT_TYPE,
+          prev: null,
+          next: null,
+          title: 'Let\'s start with some visual exercises.',
+          content: {}
+
+        };
+
+        this.PianoMusicStage = {
+          type: STAGE_TYPE.AUDIO_TYPE,
+          prev: null,
+          next: null,
+          title: 'Calming piano music.',
+          content: {}
+        }
+
+        this.UpFact3Stage = {
+          type: STAGE_TYPE.TEXT_TYPE,
+          prev: null,
+          next: null,
+          title: 'Did you know?',
+          content: {}
+        }
+
+        this.DanceVidStage = {
+          type: STAGE_TYPE.VIDEO_TYPE,
+          prev: null,
+          next: null,
+          title: 'Get up and moving to lift up your mood!',
+          content: {}
+        }
+
+        this.FunnyPicStage = {
+          type: STAGE_TYPE.PIC_TYPE,
+          prev: null,
+          next: null,
+          title: 'Funny picture',
+          content: {}
+        }
+
+        this.UpFact4Stage = {
+          type: STAGE_TYPE.TEXT_TYPE,
+          prev: null,
+          next: null,
+          title: 'Did you know?',
+          content: {}
+        }
     }
 }
